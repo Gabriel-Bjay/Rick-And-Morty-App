b2cc824ce8d3d2774fda9144d3efbcbbd6bc0eb1
Bjay <jayanjay2018@gmail.com>
2023-04-25 09:27:52 +0300

    Added The Cards Including The Different Characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
+import Card from "./components/Card/Card";
+import Filter from "./components/Filter/Filter";
+import Pagination from "./components/Pagination/Pagination";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+
+
+
 
 function App() {
+  const api = `https://rickandmortyapi.com/api/character/?page=1`
+
+  const [fetchedData, setFetchedData] = useState([]);
+  const {info,results} = fetchedData;
+
+  useEffect(() => {
+    (async  () => {
+      let data = await fetch(api)
+      .then((res) => res.json());
+      setFetchedData(data);
+    })();
+  }, [api]);
+
   return (
-      <>
-       <h1>Rick and Morty WIKI</h1>
-      </>
+    <div className="App">
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+            <div className="col-lg-8 col-12">
+              <div className="row">
+                <Card results={results} />
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
